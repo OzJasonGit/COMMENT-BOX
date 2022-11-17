a9bac880dcf6d34d34bbce88b062c56b4bece909
OzJasonGit <108492612+OzJasonGit@users.noreply.github.com>
2022-11-17 14:27:28 +0000

    more updates

--- index.js
+++ index.js
@@ -1,25 +1,24 @@
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
 function render(data){
-    let html = <div class='commentBox'> <div class='leftPanelling'> 
-    <img src='https://placeholdit.img.net/~text?w=100&h=100'/> </div> 
-    <div class='rightPanel'> <span> "+ data.name +" </span> <div class='date'> "+ data.date +" </div>
-    <p> "+ data.body +" </p></div><div class='clear'></div></div>"
-    $(#'container').append(html);
+    let html = <div class='commentBox'> 
+                    <div class='leftPanelling'> 
+                    </div> 
+
+                    <div class='rightPanel'> 
+                        <span> "+ data.name +" </span> 
+                        <div class='date'> "+ data.date +" </div>
+                    <p> "+ data.body +" </p>
+                    </div>
+                    
+                    <div class='clear'></div>
+                </div>
+    $(#'COMMENTS').append(html);
 }
+
+
+
 
 
 
@@ -31,9 +30,14 @@ $(document).ready(function(){
     }
 
     $(#'addcomment').click(function(){       
-        let addObject = {"name":,
-                         "email":,
-                         "commentText":}
+        let addObject = {
+            "name": $('#name').val(),
+            "email":$('#email').val(),
+            "commentText":$('#commentText').val()
+        };
+        console.log(addObject);
+        comment.push(addObject);
+        render(addObject);
 
     });
 
