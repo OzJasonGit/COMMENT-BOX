dadb7e6b2e684fa917beb547b67e027b5d9e32a8
OzJasonGit <108492612+OzJasonGit@users.noreply.github.com>
2022-11-18 13:40:42 +0000

    js updates

--- index.js
+++ index.js
@@ -1,53 +1,20 @@
 
 
-//
-function render(data){
-    let html = <div id='COMMENT-BOX'> 
-                    
-                    <div id='COMMENTS-APPEND'> 
-                        <span> "+ data.name +" </span> 
-                        <div id='ACTUAL-DATE'> "+ data.date +" </div>
-                        <p> "+ data.body +" </p>
-                    </div>
-                    
-                    <div class='clear'></div>
-                </div>
-    $('COMMENTS').append(html);
-}
+
+
+let inputOne = document.getElementById('inputStatus').value;
+
+let inputTwo = document.getElementById('inputStatus').value;
 
 
 
 
 
-
-$(document).ready(function(){
-    const comment = [ {name:"", email:"", commentText:""} ];
-
-    for (let i=0; i<comment.length; i+=1){
-        render(comment[i]);   
-    }
-
-    $('SUBMIT-COMMENT').click(function(){       
-        let addObject = {
-            "name": $('#name').val(),
-            "date": $('#date').val(),
-            "email":$('#email').val(),
-            "commentText":$('#commentText').val()
-        };
-        console.log(addObject);
-        comment.push(addObject);
-        render(addObject);
-        $('#name').val('');
-        $('#date').val('dd/mm/yyyy');
-        $('#email').val('');
-    });
-
-});
+const submitButton = document.querySelector('#SUBMIT-COMMENT');
 
 
+function alertBtn() {
+    alert('I also love Javascript My name is point Dexter');
+};
 
-
-
-
-
-
+submitButton.addEventListener('click', alertBtn);
