63ae126222715f1de99793361eb6556bd0b6c3e1
OzJasonGit <108492612+OzJasonGit@users.noreply.github.com>
2022-11-26 16:03:42 +0000

    added test files

--- test2.js
+++ test2.js
@@ -40,7 +40,7 @@
 
 
 
-
+let commentArr = []
 
    
 function comment(){
@@ -49,17 +49,24 @@ function comment(){
         name:document.getElementById('NAME').value,
         email:document.getElementById('EMAIL').value,
         comment:document.getElementById('COMMENTS').value,
-    }
+    };
 
     let comment = [commentObject.name, commentObject.email, commentObject.comment ]
+
+    commentArr.unshift(comment)
    
-    let personsComment = comment
+    let personsComment = commentArr
+    document.querySelector('form').reset(); 
 
     console.log(personsComment)
 
     document.getElementById("NAME-MESSAGE").innerText = personsComment;
    }
    comment();
+
+
+   localStorage.setItem(commentArr, comment)
+   console.log(localStorage);
 
 
 
